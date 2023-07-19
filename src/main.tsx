fab96d637427ca7fd34b2003ce8da01fc881f310
davitorress <dev.davitorres@gmail.com>
2023-07-19 08:34:52 -0300

    fix: new Inputs components

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,13 @@ import { ThemeProvider, StyleSheetManager } from "styled-components";
 
 import theme from "./styles/theme";
 import GlobalStyle from "./styles/global";
-import { EditRecipe } from "./pages/admin/EditRecipe";
+import { SignIn } from "./pages/admin/SignIn";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<ThemeProvider theme={theme}>
 		<StyleSheetManager shouldForwardProp={(prop) => prop !== "theme"}>
 			<GlobalStyle />
-			<EditRecipe />
+			<SignIn />
 		</StyleSheetManager>
 	</ThemeProvider>
 );
