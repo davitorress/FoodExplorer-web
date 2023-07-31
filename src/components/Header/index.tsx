6a49b19929772486490c36cfa027793c3ba2c23a
davitorress <dev.davitorres@gmail.com>
2023-07-31 15:07:02 -0300

    feat: add Header logic with auth state

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { FiMenu, FiSearch } from "react-icons/fi";
 import { PiReceipt, PiSignOut } from "react-icons/pi";
 
 import { Menu, Logo, Input, Button } from "@/components";
 import { Container, Receipt, ReceiptButton } from "./styles";
 
+import { useAuth } from "@/hooks/useAuth";
+
 export function Header() {
+	const navigate = useNavigate();
+	const { signOut, user } = useAuth();
 	const [showMenu, setShowMenu] = useState(false);
 
 	function handleShowMenu() {
@@ -26,14 +31,22 @@ export function Header() {
 				</Input.Group>
 			</Input.Root>
 
-			<Receipt>
-				<PiReceipt />
-				<ReceiptButton>0</ReceiptButton>
-			</Receipt>
+			{!user.admin ? (
+				<Receipt>
+					<PiReceipt />
+					<ReceiptButton>0</ReceiptButton>
+				</Receipt>
+			) : (
+				<div></div>
+			)}
 
-			<Button type="button" text={`Pedidos (0)`} icon={PiReceipt} />
+			{user.admin ? (
+				<Button type="button" text="Novo prato" onClick={() => navigate("/admin/create")} />
+			) : (
+				<Button type="button" text="Pedidos" icon={PiReceipt} />
+			)}
 
-			<PiSignOut />
+			<PiSignOut onClick={() => signOut()} />
 		</Container>
 	);
 }
