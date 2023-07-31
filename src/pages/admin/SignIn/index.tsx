9fcbf99e558891184e70ece9698284de75bdb1e7
davitorress <dev.davitorres@gmail.com>
2023-07-31 15:12:50 -0300

    feat: add Admin login auth

--- src/pages/admin/SignIn/index.tsx
+++ src/pages/admin/SignIn/index.tsx
@@ -1,21 +1,52 @@
-import { Button, Input, Logo } from "@/components";
+import { useState } from "react";
+
+import { Button, Input } from "@/components";
 import { Container, Form } from "./styles";
 
+import { useAuth } from "@/hooks/useAuth";
+
+import logoAdminImg from "@/assets/logo_admin.svg";
+
 export function SignIn() {
+	const { signIn } = useAuth();
+
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+		signIn({ email, password, admin: true });
+	}
+
 	return (
 		<Container>
-			<Logo />
-			<Form>
+			<picture>
+				<img src={logoAdminImg} alt="FoodExplorer" />
+			</picture>
+
+			<Form onSubmit={handleSubmit}>
 				<h1>Faça Login</h1>
 
 				<Input.Root>
 					<Input.Label id="email" text="Email" />
-					<Input.Field id="email" type="email" placeholder="Exemplo: exemplo@exemplo.com.br" />
+					<Input.Field
+						id="email"
+						type="email"
+						placeholder="Exemplo: exemplo@exemplo.com.br"
+						value={email}
+						onChange={(event: React.ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)}
+					/>
 				</Input.Root>
 
 				<Input.Root>
 					<Input.Label id="password" text="Senha" />
-					<Input.Field id="password" type="password" placeholder="No mínimo 6 caracteres" />
+					<Input.Field
+						id="password"
+						type="password"
+						placeholder="No mínimo 6 caracteres"
+						value={password}
+						onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)}
+					/>
 				</Input.Root>
 
 				<Button type="submit" text="Entrar" />
--- src/pages/admin/SignIn/styles.ts
+++ src/pages/admin/SignIn/styles.ts
@@ -9,6 +9,18 @@ export const Container = styled.main`
 	flex-direction: column;
 	gap: 72px;
 
+	> picture {
+		width: 100%;
+		height: 100%;
+		display: block;
+
+		> img {
+			width: 100%;
+			height: 100%;
+			object-fit: contain;
+		}
+	}
+
 	@media ${({ theme }) => theme.DEVICES.MOBILE_SM} {
 		padding: 158px 48px 65px;
 	}
