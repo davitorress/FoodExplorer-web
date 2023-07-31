36d9be2e17139e32ca09fd67c3196eef69e7320b
davitorress <dev.davitorres@gmail.com>
2023-07-31 15:05:50 -0300

    feat: add Logo switch on auth state

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,12 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "@/hooks/useAuth";
+
 import logoImg from "@/assets/logo.svg";
 import logoAdminImg from "@/assets/logo_admin.svg";
 
 import { Container } from "./styles";
 
 export function Logo() {
+	const { user } = useAuth();
+	const navigate = useNavigate();
+
 	return (
-		<Container>
-			<img src={logoImg} alt="FoodExplorer" />
+		<Container onClick={() => navigate("/")}>
+			{user && user.admin ? <img src={logoAdminImg} alt="FoodExplorer" /> : <img src={logoImg} alt="FoodExplorer" />}
 		</Container>
 	);
 }
