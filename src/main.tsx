0d9c4d81a69ae395326f048ed5b12a07d5504d64
davitorress <dev.davitorres@gmail.com>
2023-07-31 15:03:37 -0300

    feat: add AuthProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import { ThemeProvider, StyleSheetManager } from "styled-components";
 import theme from "./styles/theme";
 import GlobalStyle from "./styles/global";
 import { Routes } from "./routes";
+import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<ThemeProvider theme={theme}>
 		<StyleSheetManager shouldForwardProp={(prop) => prop !== "theme"}>
 			<GlobalStyle />
-			<Routes />
+			<AuthProvider>
+				<Routes />
+			</AuthProvider>
 		</StyleSheetManager>
 	</ThemeProvider>
 );
