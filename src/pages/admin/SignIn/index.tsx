8caa6dd6c7fbe84cac75fbf1cf58626ec60d5914
davitorress <dev.davitorres@gmail.com>
2023-08-15 20:19:50 -0300

    fix: alter text for Admin SignIn

--- src/pages/admin/SignIn/index.tsx
+++ src/pages/admin/SignIn/index.tsx
@@ -27,7 +27,7 @@ export function SignIn() {
 			</picture>
 
 			<Form onSubmit={handleSubmit}>
-				<h1>Faça Login</h1>
+				<h1>Faça Login (Administrativo)</h1>
 
 				<Input.Root>
 					<Input.Label id="email" text="Email" />
